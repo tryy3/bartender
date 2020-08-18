789f5e5ce2be2e177798ac7dcf35d6380440d894
Dennis Planting <dennistryy3@gmail.com>
2020-08-19 00:46:19 +0200

    Started working on caching
    Started moving the graphql over to backend.
    
    Next step will be to change graphql on frontend to communicate with backed using tauri.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,99 +1,109 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
 )]
 
+use serde::Serialize;
 use std::error::Error;
-use serde::{Serialize};
 use std::{thread, time};
-
+mod cache;
 mod cmd;
 mod config;
 
 #[derive(Debug, Serialize)]
 struct SimpleResponse<'a> {
-    message: &'a str,
+  message: &'a str,
 }
 
 // An error type we define
 // We could also use the `anyhow` lib here
 #[derive(Debug, Clone)]
 struct CommandError<'a> {
-    message: &'a str,
+  message: &'a str,
 }
 
 impl<'a> CommandError<'a> {
-    fn new(message: &'a str) -> Self {
-        Self { message }
-    }
+  fn new(message: &'a str) -> Self {
+    Self { message }
+  }
 }
 
 impl<'a> std::fmt::Display for CommandError<'a> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message)
-    }
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}", self.message)
+  }
 }
 
 // Tauri uses the `anyhow` lib so custom error types must implement std::error::Error
 // and the function call should call `.into()` on it
 impl<'a> std::error::Error for CommandError<'a> {}
 
-fn main() -> Result<(), Box<dyn Error>> {
-    tauri::AppBuilder::new()
-        .invoke_handler(|_webview, arg| {
-            use cmd::cmd::Cmd::*;
-            match serde_json::from_str(arg) {
-                Err(e) => Err(e.to_string()),
-                Ok(command) => {
-                    match command {
-                        PourDrink { payload, callback, error } => tauri::execute_promise(
-                            _webview,
-                            move || {
-                                thread::sleep(time::Duration::from_millis(5000));
-                                println!("{:?}", payload);
-                                let response = SimpleResponse {
-                                    message: "Finished",
-                                };
-                                Ok(response)
-                            },
-                            callback,
-                            error,
-                        ),
-                        LoadSettings { callback, error } => tauri::execute_promise(
-                            _webview, 
-                            move || {
-                                match config::config::read_config() {
-                                    Err(why) => {
-                                        println!("Unable to load config: {}", why);
-                                        Err(CommandError::new("Unable to load config").into())
-                                    },
-                                    Ok(settings) => Ok(settings)
-                                }
-                            },
-                            callback, 
-                            error
-                        ),
-                        SaveSettings { payload, callback, error } => tauri::execute_promise(
-                            _webview, 
-                            move || {
-                                match config::config::save_config(payload) {
-                                    Err(why) =>{
-                                        println!("unable to save config: {}", why);
-                                        Err(CommandError::new("Unable to save config").into())
-                                    },
-                                    Ok(_) => Ok(SimpleResponse{message:"Successfully saved the config"})
-                                }
-                            },
-                            callback, 
-                            error
-                        )
-                    }
-                    Ok(())
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+  let c = cache::cache::Cache::new();
+  println!("{:?}", c);
+
+  tauri::AppBuilder::new()
+    .invoke_handler(|_webview, arg| {
+      use cmd::cmd::Cmd::*;
+      match serde_json::from_str(arg) {
+        Err(e) => Err(e.to_string()),
+        Ok(command) => {
+          match command {
+            PourDrink {
+              payload,
+              callback,
+              error,
+            } => tauri::execute_promise(
+              _webview,
+              move || {
+                thread::sleep(time::Duration::from_millis(5000));
+                println!("{:?}", payload);
+                let response = SimpleResponse {
+                  message: "Finished",
+                };
+                Ok(response)
+              },
+              callback,
+              error,
+            ),
+            LoadSettings { callback, error } => tauri::execute_promise(
+              _webview,
+              move || match config::config::read_config() {
+                Err(why) => {
+                  println!("Unable to load config: {}", why);
+                  Err(CommandError::new("Unable to load config").into())
                 }
-            }
-        })
-        .build()
-        .run();
-    
-    Ok(())
+                Ok(settings) => Ok(settings),
+              },
+              callback,
+              error,
+            ),
+            SaveSettings {
+              payload,
+              callback,
+              error,
+            } => tauri::execute_promise(
+              _webview,
+              move || match config::config::save_config(payload) {
+                Err(why) => {
+                  println!("unable to save config: {}", why);
+                  Err(CommandError::new("Unable to save config").into())
+                }
+                Ok(_) => Ok(SimpleResponse {
+                  message: "Successfully saved the config",
+                }),
+              },
+              callback,
+              error,
+            ),
+          }
+          Ok(())
+        }
+      }
+    })
+    .build()
+    .run();
+
+  Ok(())
 }
